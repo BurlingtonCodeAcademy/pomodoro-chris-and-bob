6c94c9ed179ce9f236d8f9f53c714f95814aa657
Habenzy <robertrstauss@gmail.com>
2018-10-19 15:15:16 -0400

    re-wrote countdown function from scratch, and modified other functions to work, complete functionality!!!! no ending flash or noise yet

--- pomodoro.js
+++ pomodoro.js
@@ -1,59 +1,55 @@
 const pauseButton = document.getElementById('pause');
 const startButton = document.getElementById('start');
 const resetButton = document.getElementById('reset');
-let currentTime = parseInt(document.getElementById('clock').innerHTML);
+let currentTime = 1500;
+let minutesNow = Math.floor(currentTime / 60);
+let secondsNow = currentTime % 60;
 let paused = false;
 console.log(currentTime)
-window.open(startButton.onclick = startTimer);
-window.open(pauseButton.disabled = true);
-window.open(resetButton.disabled = true);
-window.open(pauseButton.onclick = pauseTimer);
-window.open(resetButton.onclick = resetTimer);
+startButton.onclick = startTimer;
+pauseButton.disabled = true;
+resetButton.disabled = true;
+pauseButton.onclick = pauseTimer;
+resetButton.onclick = resetTimer;
 
-function countdown(mins) {
-  if (paused === false) {
-    let seconds = 59;
-    let minutes = mins;
-    function tick() {
-      let clock = document.getElementById('clock');
-      let currentMins = minutes - 1
-
-      clock.innerHTML = currentMins.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
-      if (seconds >= 0) {
-        seconds--;
-        timer = setTimeout(tick, 1000);
+//track time by seconds countdown takes sec, 25min = 1500sec, display in min:sec using remainder to get Mins
+//sec = time(sec)%60 min = time(sec)/60
+function countdown(secs) {
+  if (currentTime > 0) {
+    let seconds = document.getElementById('seconds');
+    let minutes = document.getElementById('minutes');
+    if (paused === false) {
+      currentTime--;
+      if (parseInt(seconds.innerHTML) <= 10  && parseInt(seconds.innerHTML) != 0) {
+        seconds.innerHTML = '0' + String(currentTime % 60);
       }
       else {
-        if (mins > 1) {
-          countdown(mins - 1);
-        }
-      }
+      seconds.innerHTML = String(currentTime % 60);
+      };
+      minutes.innerHTML = String(Math.floor(currentTime / 60));
     }
-    tick();
   }
 };
 
 function pause() {
   paused = true;
-  clearTimeout(timer);
-  let timeNow = parseInt(document.getElementById('clock').innerHTML);
-  let secondsNow = document.getElementById('seconds');
-  currentTime = (timeNow + 1) + secondsNow;
-  console.log(currentTime)
+  clearInterval(timer);
 };
 
 function reset() {
-  pause();
-  document.getElementById('clock').innerHTML = '25:00';
-  currentTime = 25;
+  clearInterval(timer);
+  currentTime = 1500;
+  document.getElementById('minutes').innerHTML = String(Math.floor(currentTime / 60));
+  document.getElementById('seconds').innerHTML = String(currentTime % 60) + '0';
 };
 
 function startTimer() {
+  timer = setInterval(countdown, 100);
   paused = false;
   startButton.disabled = true;
-  countdown(currentTime);
   pauseButton.disabled = false;
   resetButton.disabled = false;
+  countdown(currentTime);
 };
 
 function pauseTimer() {
